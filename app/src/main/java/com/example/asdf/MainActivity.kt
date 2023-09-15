a0f79f187a425eef65c943e0f8f15a0e173fc454
jankowskyy <konrad.maksymilian.jankowski@gmail.com>
2023-09-15 09:41:00 +0200

    .

--- app/src/main/java/com/example/asdf/MainActivity.kt
+++ app/src/main/java/com/example/asdf/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.asdf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.Zatwierdz).setOnClickListener{
+            findViewById<TextView>(R.id.Wynik).text =
+            findViewById<TextInputLayout>(R.id.Imie).text + findViewById<TextInputLayout>(R.id.Nazwisko).text
+
+        }
     }
 }
