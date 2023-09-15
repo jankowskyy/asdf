879c396b1070085b66a498371aa55e8150832880
jankowskyy <konrad.maksymilian.jankowski@gmail.com>
2023-09-15 09:57:17 +0200

    .

--- app/src/main/java/com/example/asdf/MainActivity.kt
+++ app/src/main/java/com/example/asdf/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.get
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.Zatwierdz).setOnClickListener{
             findViewById<TextView>(R.id.Wynik).text =
-            findViewById<TextInputLayout>(R.id.Imie).text + findViewById<TextInputLayout>(R.id.Nazwisko).text
+                (findViewById<TextInputLayout>(R.id.Imie).text) + (findViewById<TextInputLayout>(R.id.Nazwisko).text)
 
         }
     }
